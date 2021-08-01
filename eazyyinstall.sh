7a907e814705b25882173bd1583f0c04e075a3dd
Jitul Kumar Laphong <jitul.kumar3113@hmail.com>
2021-08-01 22:44:07 +0530

    Added subkime text

--- eazyyinstall.sh
+++ eazyyinstall.sh
@@ -292,6 +292,7 @@ sublime-text(){
 
 			update
 			sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+			sudo snap install curl
 			curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 			sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 			sudo apt install sublime-text
